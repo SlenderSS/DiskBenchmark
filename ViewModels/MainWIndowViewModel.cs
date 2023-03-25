08d98865263fb04aa679dcab673e23e743afd0bc
Oleksandr Olkhovyk <oleksandr.olkhovyk.ki.2020@lpnu.ua>
2023-03-25 11:57:29 +0200

    Close app command

--- ViewModels/MainWIndowViewModel.cs
+++ ViewModels/MainWIndowViewModel.cs
@@ -1,16 +1,42 @@
-﻿using System;
+﻿using DiskBenchmark.Infrastructure.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
 
 namespace DiskBenchmark.ViewModels
 {
     internal class MainWIndowViewModel
     {
 
+        #region Commands
+
+        #region CloseApp
+        private ICommand CloseAppCommand { get; }
+
+
+        private void OnCloseAppCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+        private bool CanCloseAppCommandExecute(object p) => true;
+
+
+        #endregion
+
+        #endregion
         public MainWIndowViewModel()
         {
+            #region Commands
+            CloseAppCommand = new LambdaCommand(OnCloseAppCommandExecuted, CanCloseAppCommandExecute);
+            #endregion
+
+
+
+
 
         }
     }
