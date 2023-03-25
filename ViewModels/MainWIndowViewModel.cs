f9f5a4f1556f792c3d62cdbef50aa5a878b04469
Oleksandr Olkhovyk <oleksandr.olkhovyk.ki.2020@lpnu.ua>
2023-03-25 11:39:00 +0200

    Created ViewModel for main window

--- ViewModels/MainWIndowViewModel.cs
+++ ViewModels/MainWIndowViewModel.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DiskBenchmark.Infrastructure.Commands
+namespace DiskBenchmark.ViewModels
 {
-    internal class Command : 
+    internal class MainWIndowViewModel
     {
+
+        public MainWIndowViewModel()
+        {
+
+        }
     }
 }
