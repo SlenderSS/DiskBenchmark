98c9ecb17529007daa236530e9b2b9e44e020ee6
Alex Olkhovyk <oleksandr.olkhovyk.ki.2020@lpnu.ua>
2023-04-01 13:34:09 +0300

    fixed bytes converter

--- Infrastructure/Converters/BytesToGigabytes.cs
+++ Infrastructure/Converters/BytesToGigabytes.cs
@@ -12,15 +12,10 @@ namespace DiskBenchmark.Infrastructure.Converters
         private long _gigabyte = 1024 * 1024 * 1024;
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (!(value is double bytes)) return null;
+            if (!(value is long bytes)) return null;
 
             return bytes / _gigabyte;
         }
-        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (!(value is double gigabytes)) return null;
-
-            return gigabytes * _gigabyte;
-        }
+        
     }
 }
